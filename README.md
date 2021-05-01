ab8f869687323d31680695b0abb8bd1a4b188a4a
titansrule3035 <54777485+titansrule3035@users.noreply.github.com>
2021-05-01 19:18:08 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# projectAlgorithmDesign
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,29 @@
+var fixedRect, movingRect, fixedRect1;
+function setup(){
+  createCanvas(1200,800);
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  fixedRect1 = createSprite(200, 100, 50, 50);
+  fixedRect1.shapeColor = "green"; 
+}
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if(isTouching(movingRect, fixedRect)){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+  else if(isTouching(movingRect, fixedRect1)){
+    movingRect.shapeColor = "red";
+    fixedRect1.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+    fixedRect1.shapeColor = "green";
+  }
+  drawSprites();
+}
